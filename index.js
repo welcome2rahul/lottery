78c477cbf3746f6587252f2d611cee3c5edec510
Rahul Kumar <75586687+welcome2rahul@users.noreply.github.com>
2023-11-02 11:56:45 +0530

    index,js

---
+++ index.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import configViewEngine from './src/config/configEngine';
+import routes from './src/routes/web';
+import cronJobContronler from './src/controllers/cronJobContronler';
+import socketIoController from './src/controllers/socketIoController';
+require('dotenv').config();
+let cookieParser = require('cookie-parser');
+
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+const port = process.env.PORT || 80;
+
+app.use(cookieParser());
+// app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// setup viewEngine
+configViewEngine(app);
+// init Web Routes
+routes.initWebRouter(app);
+
+// Cron game 1 Phut 
+cronJobContronler.cronJobGame1p(io);
+
+// Check xem ai connect vào sever 
+socketIoController.sendMessageAdmin(io);
+
+// app.all('*', (req, res) => {
+//     return res.render("404.ejs"); 
+// });
+
+server.listen(port, () => {
+    console.log("Connected success port: " + port);
+});
+
